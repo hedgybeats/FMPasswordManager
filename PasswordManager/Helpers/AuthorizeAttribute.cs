7dee4864fe7e5ce5429b4d3f23db605b785f421c
Layton Hedges <Layton.Hedges@fostermelliar.co.za>
2022-03-15 11:46:15 +0200

    WIP

---
+++ PasswordManager/Helpers/AuthorizeAttribute.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using PasswordManager.Models;
+using System;
+
+namespace PasswordManager.Helpers
+{
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
+    public class AuthorizeAttribute : Attribute, IAuthorizationFilter
+    {
+        public void OnAuthorization(AuthorizationFilterContext context)
+        {
+            var user = (User)context.HttpContext.Items["User"];
+            if (user == null)
+            {
+                // not authenticated
+                context.Result = new JsonResult(new { message = "Unauthorized" }) { StatusCode = StatusCodes.Status401Unauthorized };
+            }
+        }
+    }
+}
