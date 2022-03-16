c092d89c12f75d0e8c6a89052c9c3ed3f52afca9
Marco <marcodumbleton1@gmail.com>
2022-03-16 09:58:55 +0200

    added a method for the JWT token in user service

--- PasswordManager/Services/Interfaces/IUserService.cs
+++ PasswordManager/Services/Interfaces/IUserService.cs
@@ -1,4 +1,6 @@
 ﻿using PasswordManager.DTOs;
+using PasswordManager.Models;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace PasswordManager.Services.Interfaces
@@ -6,5 +8,10 @@ namespace PasswordManager.Services.Interfaces
     public interface IUserService
     {
         Task<int> AddUser(AddUserDTO addUserDto);
+        Task DeleteUser(int id);
+        Task<User> GetUser(int id);
+        Task<List<User>> GetUsers();
+        Task PutUser(int id, User user);
+        string GenerateJwtToken(int userId);
     }
 }
